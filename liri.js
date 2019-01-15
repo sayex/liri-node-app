273997e9f1c29c877f5ac4cadca5970b2020c34f
sayex <elsayer@msn.com>
2019-01-14 18:01:21 -0700

    using axios for get calls

--- liri.js
+++ liri.js
@@ -1,18 +1,14 @@
 require("dotenv").config();
-require("Axios");
-require("moment");
-
+var axios = require("Axios");
+var moment = require("moment");
 var keys = require("./keys.js");
 
-var omdb = keys.omdb
-var BitKey = keys.bit
+var omdb = keys.omdb.key;
+var bitKey = keys.bit.key;
 var Spotify = require('node-spotify-api');
 
+
 var spotify = new Spotify(keys.spotify);
-console.log(keys)
-console.log(spotify);
-console.log(omdb);
-console.log(BitKey);
 
 var searchEndPoint = process.argv[2];
 var search = process.argv[3];
@@ -50,15 +46,45 @@ switch (searchEndPoint) {
 }
 
 function searchSpotify() {
-    console.log(search);
+
+    spotify.search({
+        type: 'track',
+        query: search
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var results = data.items
+        console.log(data.tracks.items)
+        // for (i = 0; i < data.tracks.length; i++) {
+        //     console.log(data.tracks[i]);
+        // }
+        // console.log(data.tracks)
+    });
 }
 
 function searchBands() {
-    console.log(search);
+    axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=" + bitKey)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+
+
 }
 
 function searchOMDB() {
-    console.log(search);
+    axios.get('http://www.omdbapi.com/?apikey=' + omdb + '&s=' + search)
+        .then(function (response) {
+            resp = response
+            console.log(resp);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 function sayWhat() {
